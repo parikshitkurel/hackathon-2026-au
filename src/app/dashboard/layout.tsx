aef51f410b9f4005d5bf394610af5aa958735130
parikshitkurel <parikshitkurel@gmail.com>
2026-05-13 18:14:21 +0530

    Fixing Navbar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,14 +1,11 @@
-import { Navbar } from "@/components/layout/Navbar";
-
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen flex-col bg-background">
-      <Navbar />
-      <main className="flex-1">{children}</main>
+    <div className="flex-1 flex flex-col">
+      {children}
     </div>
   );
 }
